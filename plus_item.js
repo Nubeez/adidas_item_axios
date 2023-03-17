892839c93108c25d968849fa65d4f79d56c41107
Sungho Park <nonckk@gmail.com>
2023-03-17 12:36:53 +0900

    Create plus_item.js

---
+++ plus_item.js
@@ -0,0 +1,68 @@
+[
+    {
+    "id": 0,
+
+    "title": "ADIDAS WEAR",
+
+    "content": "다시 돌아온 Adidas 14.1oz",
+
+    "buy": "지금 구매하기",
+
+    "imgUrl": "img/teaser/IMG_(0).jpg",
+    },
+    {
+    "id": 1,
+
+    "title": "ADIDAS Shoes",
+
+    "content": "스트릿패션 Adidas Walker",
+
+    "buy": "지금 구매하기",
+
+    "imgUrl": "img/teaser/IMG_(1).mp4",
+    },
+    {
+    "id": 2,
+
+    "title": "ADIDAS Shoes",
+
+    "content": " 퍼포먼스 Adidas Ozweego TR",
+
+    "buy": "지금 구매하기",
+
+    "imgUrl": "img/teaser/IMG_(2).jpg",
+    },
+    {
+    "id": 3,
+
+    "title": "ADIDAS WEAR",
+
+    "content": "아디다스 바스켓볼의 새로운 컬렉션 Adidas kid's",
+
+    "buy": "지금 구매하기",
+
+    "imgUrl": "img/teaser/IMG_(3).mp4",
+    },
+    {
+    "id": 4,
+
+    "title": "ADIDAS WEAR",
+
+    "content": "스타일리쉬 Adidas Women's",
+
+    "buy": "지금 구매하기",
+
+    "imgUrl": "img/teaser/IMG_(4).jpg",
+    },
+    {
+    "id": 5,
+
+    "title": "ADIDAS Shoes",
+
+    "content": "심플 & 베이직 Adidas ",
+
+    "buy": "지금 구매하기",
+
+    "imgUrl": "/img/teaser/IMG_(5).jpg",
+    },
+];
